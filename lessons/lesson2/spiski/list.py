85e2bf902b40d8649f0973dc55b473f8588b80e8
petr <panisimov@gmail.com>
2023-05-08 10:30:58 +0300

    lesson 2 - работа со списками

--- lessons/lesson2/spiski/list.py
+++ lessons/lesson2/spiski/list.py
@@ -1,1 +1,13 @@
-quarter = ['January','February','March']
+quarter = ['January', 'February', 'March']
+
+print('First Month: ', quarter[0])
+print('Second Month: ', quarter[1])
+print('Third Month: ', quarter[2])
+
+#инструкция для создания многомерного списка из 2 элементов, каждый их которыйх
+#является списком из трех элементов, содержащих целое значение
+
+coords = [[1,2,3],[4,5,6]]
+
+print('\nTop Left 0,0 :', coords[0][0])
+print('Bottom Right 1,2 :', coords[1][2])
